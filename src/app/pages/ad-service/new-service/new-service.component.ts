8200e522d774d27484fe822bb8dab844c94dd0a5
alex.smit <alexdino2@gmail.com>
2022-09-17 16:16:36 +0200

    ad-service meg search-service alap

---
+++ src/app/pages/ad-service/new-service/new-service.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-service',
+  templateUrl: './new-service.component.html',
+  styleUrls: ['./new-service.component.scss']
+})
+export class NewServiceComponent implements OnInit {
+
+  regForm = new FormGroup({
+    name: new FormControl(''),
+    password: new FormControl(''),
+    rePassword: new FormControl(''),
+    email: new FormControl(''),
+    tel: new FormControl(''),
+    rank: new FormControl('')
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.regForm.value);
+  }
+
+
+}
---
+++ src/app/shared/models/Basicdatabase.ts
@@ -0,0 +1,10 @@
+export interface Basicdatabase {
+    id: number;
+    service: string;
+    price: number;
+    status: string;
+    buttons: string;
+    name: string;
+    phone: string;
+    email: string;
+}
